1d5726e6bf69a10ca6ce7cc27cd8194e2921727c
Denis Holdeew <h.denis@gmx.de>
2020-06-06 13:25:08 +0200

    Test github actions exec toolkit

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const exec = require('@actions/exec');
 
 try {
   // `who-to-greet` input defined in action metadata file
@@ -9,6 +10,7 @@ try {
   core.setOutput("time", time);
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2)
+  await exec.exec('sh test.sh');
   console.log(`The event payload: ${payload}`);
 } catch (error) {
   core.setFailed(error.message);
