cebdfde37e95a845b036a06fed377705fd38543a
Denis Holdeew <h.denis@gmx.de>
2020-06-06 17:41:32 +0200

    ...

--- index.js
+++ index.js
@@ -5,13 +5,14 @@ const exec = require('@actions/exec');
 try {
   // `who-to-greet` input defined in action metadata file
   const nameToGreet = core.getInput('who-to-greet');
-  await exec.exec('sh test.sh');
-  console.log(`Hello there, ${nameToGreet}!!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2);
-  console.log(`The event payload: ${payload}`);
+  exec.exec('sh test.sh'). then(() => {
+    console.log(`Hello there there, ${nameToGreet}!!!`);
+    const time = (new Date()).toTimeString();
+    core.setOutput("time", time);
+    // Get the JSON webhook payload for the event that triggered the workflow
+    const payload = JSON.stringify(github.context.payload, undefined, 2);
+    console.log(`The event payload: ${payload}`);
+  });
 } catch (error) {
   core.setFailed(error.message);
 }
