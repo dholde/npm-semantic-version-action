64bd51108696040e603179762459ab02c96a3ae6
Denis Holdeew <h.denis@gmx.de>
2020-06-06 14:14:07 +0200

    Dummy change

--- dist/index.js
+++ dist/index.js
@@ -4704,7 +4704,7 @@ const github = __webpack_require__(148);
 try {
   // `who-to-greet` input defined in action metadata file
   const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello here ${nameToGreet}!`);
+  console.log(`Hello there,  ${nameToGreet}!`);
   const time = (new Date()).toTimeString();
   core.setOutput("time", time);
   // Get the JSON webhook payload for the event that triggered the workflow
