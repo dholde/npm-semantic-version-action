d61d06c26b12ca6de63bf04ce2c68bd9e6aa3664
Denis Holdeew <h.denis@gmx.de>
2020-06-06 17:10:18 +0200

    Add exec action toolkit

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const exec = require('@actions/exec');
 
 try {
   // `who-to-greet` input defined in action metadata file
@@ -8,7 +9,8 @@ try {
   const time = (new Date()).toTimeString();
   core.setOutput("time", time);
   // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
+  const payload = JSON.stringify(github.context.payload, undefined, 2);
+  await exec.exec('sh test.sh');
   console.log(`The event payload: ${payload}`);
 } catch (error) {
   core.setFailed(error.message);
